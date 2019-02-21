290b6b8dd5112e5fb47e167a1bd6143c00c70676
supportpls <dikonnikov@gmail.com>
2019-02-21 19:30:09 +1300

    2nd upload

---
+++ ImportResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GalaxyCinemas
+{
+    public class ImportResult
+    {
+		public int TotalRows { set; get;}
+		public int ImportedRows{ set; get;}
+		public int FailedRows{ set; get;}
+		private List<String> errorMessages {set;get;}
+
+
+		
+		public List<String> ErrorMessages 
+		{
+			get 
+			{
+				return errorMessages;
+			}
+		}
+       
+		public ImportResult(){
+			TotalRows = 0;
+			ImportedRows = 0;
+			FailedRows = 0;
+			errorMessages.Clear();
+
+		}
+    }
+}
