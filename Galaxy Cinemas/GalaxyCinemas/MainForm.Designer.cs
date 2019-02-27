b98dc2a0fa0740d1f95900b2f803b9e69ff79933
Dan <dikonnikov@gmail.com>
2019-02-27 20:41:23 +1300

    Add files via upload

--- Galaxy Cinemas/GalaxyCinemas/MainForm.Designer.cs
+++ Galaxy Cinemas/GalaxyCinemas/MainForm.Designer.cs
@@ -30,13 +30,17 @@
         {
             this.btnImport = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBooking = new System.Windows.Forms.Button();
+            this.btnExportForm = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(43, 157);
+            this.btnImport.Location = new System.Drawing.Point(34, 120);
             this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(196, 23);
+            this.btnImport.Size = new System.Drawing.Size(231, 23);
             this.btnImport.TabIndex = 0;
             this.btnImport.Text = "Import Movies and Sessions ";
             this.btnImport.UseVisualStyleBackColor = true;
@@ -45,17 +49,61 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("SketchFlow Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(38, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.Size = new System.Drawing.Size(0, 31);
             this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Galaxy Cinemas";
+            // 
+            // btnBooking
+            // 
+            this.btnBooking.Location = new System.Drawing.Point(34, 166);
+            this.btnBooking.Name = "btnBooking";
+            this.btnBooking.Size = new System.Drawing.Size(231, 23);
+            this.btnBooking.TabIndex = 6;
+            this.btnBooking.Text = "Make a Booking";
+            this.btnBooking.UseVisualStyleBackColor = true;
+            this.btnBooking.Click += new System.EventHandler(this.btnBooking_Click);
+            // 
+            // btnExportForm
+            // 
+            this.btnExportForm.Location = new System.Drawing.Point(34, 215);
+            this.btnExportForm.Name = "btnExportForm";
+            this.btnExportForm.Size = new System.Drawing.Size(231, 23);
+            this.btnExportForm.TabIndex = 7;
+            this.btnExportForm.Text = "Export Movies and Sessions";
+            this.btnExportForm.UseVisualStyleBackColor = true;
+            this.btnExportForm.Click += new System.EventHandler(this.btnExportForm_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(103, 405);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "QUIT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(321, 458);
+            this.ClientSize = new System.Drawing.Size(307, 458);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnExportForm);
+            this.Controls.Add(this.btnBooking);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnImport);
             this.Name = "MainForm";
@@ -69,5 +117,9 @@
 
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBooking;
+        private System.Windows.Forms.Button btnExportForm;
+        private System.Windows.Forms.Button button4;
     }
 }
