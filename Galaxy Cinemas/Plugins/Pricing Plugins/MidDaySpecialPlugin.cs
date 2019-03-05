bebe62b16857f499c5ccf1a87c9caafc596aaf24
di <dikonnikov@gmail.com>
2019-03-05 19:31:53 +1300

    catchin' bugs 2019-03-05

--- Galaxy Cinemas/Plugins/Pricing Plugins/MidDaySpecialPlugin.cs
+++ Galaxy Cinemas/Plugins/Pricing Plugins/MidDaySpecialPlugin.cs
@@ -15,16 +15,19 @@ namespace GalaxyCinemas
         public bool CalculateSpecial(Booking booking, ref string specialName, ref decimal specialPrice)
         {
             TimeSpan timeOfDay = booking.SessionDate.TimeOfDay;
+            TimeSpan elevenAM = new TimeSpan(11, 00, 00);
+            TimeSpan onePM = new TimeSpan(13, 00, 00);
+
             // If not mid-day, not applicable.
             // If movie doesn't start between 11am and 1pm
-			if((timeOfDay.CompareTo(TimeSpan(11,0,0))<0)||(timeOfDay.CompareTo(TimeSpan(11,0,0))>0))
+            if ((timeOfDay.CompareTo(elevenAM) <0)||(timeOfDay.CompareTo(onePM) >0))
 			{
 				return false;
 			}
 			else 
 			{
 	            // Calculate the discounted price that we would offer.
-				decimal discountedPrice = booking.OriginalPrice*0.8;
+				decimal discountedPrice = booking.OriginalPrice*0.8M;
 				// If this discount is applicable, set specialName and specialPrice to our name and price.
 				if (discountedPrice < booking.OriginalPrice) 
 				{
